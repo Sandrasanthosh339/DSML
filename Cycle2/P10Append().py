74bf567bfd42f18366ac70a6c9b66be5dd439873
Sandrasanthosh339 <74653003+Sandrasanthosh339@users.noreply.github.com>
2021-12-06 11:19:46 +0530

    Update P10Append().py

--- Cycle2/P10Append().py
+++ Cycle2/P10Append().py
@@ -1,13 +1,16 @@
 import numpy as np
 
 ar = np.array([1, 2, 3, 4, 5, 6, 7])
+a = np.append(ar,6)
+print("1D array",ar)
+print("the use of append() function in 1D array \n",a)
 
-a = np.append(ar, 81)
-print("1D Array Diagonal values : \n", a)
  
-ar = np.array( [ [1, 2, 3],[ 4, 5, 6] ])
-print("2D arr : \n", ar)
+ar1 = np.array( [ [1, 2, 3],[ 4, 5, 6] ])
+print("2D array : \n", ar1)
 
-a = np.append(ar, [22, 23, 24])
-print("2D Array diagonal values : \n", a)
-
+a = np.append(ar1,np.array([[7,8,9]]),axis=0)
+print("\n2D array")
+print(ar1)
+print("the use of append() function in 2D array")
+print(a)
