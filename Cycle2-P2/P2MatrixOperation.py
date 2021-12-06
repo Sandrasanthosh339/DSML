f6116739f4df088c7f01fb7a3b1994d078c0452d
Sandrasanthosh339 <74653003+Sandrasanthosh339@users.noreply.github.com>
2021-12-06 12:35:04 +0530

    Update P2MatrixOperation.py

--- Cycle2-P2/P2MatrixOperation.py
+++ Cycle2-P2/P2MatrixOperation.py
@@ -1,15 +1,19 @@
 import numpy as np
 m=np.arange(1,10).reshape(3,3)
-print(m)
+print("Matrix:\n ",m)
 
-print("Cube using multiply():")
-print(np.multiply(m,3))
+print("Cube using multiply():\n",np.multiply(m,3))
 
-print("Cube using * ")
-print(m*m*m)
+print("Cube using * :\n ",m*m*m)
 
-print("Cube using power(): ")
-print(np.power(m,3))
+print("Cube using power():\n",np.power(m,3))
 
-print("Cube using ** : ")
-print(m**3)
+print("Cube using ** :\n",m**3)
+
+print("Identity matrix of the given square matrix:")
+print(np.identity(3,dtype= int))
+
+print(" Display each element of the matrix to different powers.")
+print(np.power(m,m))
+
+print("Create a matrix Y with same dimension as X and perform the operation X2+2Y:",)
